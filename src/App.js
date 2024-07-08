2c0e00f4ce7efeac317c41a7870a35d0c9a95ca6
wasori <wasori@naver.com>
2024-07-08 17:49:25 +0900

    최초등록

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = 'ReactBlog';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
+  // let [글제목2, c] = useState('강남 우동 맛집');
+  // let [글제목3, d] = useState('파이썬 독학');
+
+  let [따봉, 따봉변경] = useState(0);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>{ post }</h4>
+      </div>
+
+      <button onClick={()=>{ 
+          let copy = [...글제목];
+          copy.sort();
+          글제목변경(copy);
+        }}>가나다순정렬</button>
+
+      <button onClick={()=>{ 
+          let copy = [...글제목];
+          copy[0] = '여자 코트 추천';
+          글제목변경(copy);
+        }}>변경</button>
+
+      <div className="list">
+        <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> { 따봉 } </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      
+      <Modal></Modal>
+
+    </div>
+  );
+}
+
+function Modal(){
+  return(
+    <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
     </div>
   );
 }
